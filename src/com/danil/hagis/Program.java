7a78b540944b68cd82805c36df8a48b973cfa553
Danila2510 <mtedia200323@gmail.com>
2024-09-17 13:18:11 +0300

    Initial commit

---
+++ src/com/danil/hagis/Program.java
@@ -0,0 +1,14 @@
+package com.danil.hagis;
+public class Program {
+
+	public static void main(String[] args) {
+		 int[] myArray = {10, 11, 12, 13, 14};
+	        for (int i = 0; i < myArray.length; i++) {
+	            System.out.println("Массив: " + myArray[i]);
+	     
+	}
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.println("Цикл: " + i);
+	        }
+}
+}
